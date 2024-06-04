31bfedecb8af584ccfe868280e0dd5ad92894dbe
Gabriel Schincariol <gschin11@gmail.com>
2024-06-03 22:26:30 -0300

    Resolver warnings

--- frontend/app/src/main/java/br/com/fiap/mareco/components/DestaquesComponent.kt
+++ frontend/app/src/main/java/br/com/fiap/mareco/components/DestaquesComponent.kt
@@ -1,7 +1,6 @@
 package br.com.fiap.mareco.components
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
@@ -15,7 +14,6 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
--- frontend/app/src/main/java/br/com/fiap/mareco/screens/HomeScreen.kt
+++ frontend/app/src/main/java/br/com/fiap/mareco/screens/HomeScreen.kt
@@ -6,25 +6,16 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import br.com.fiap.mareco.R
 import br.com.fiap.mareco.components.DestaqueComponent
-import br.com.fiap.mareco.model.CardConfiguracao
-import br.com.fiap.mareco.model.Destaque
 import br.com.fiap.mareco.services.listarReportes
-import br.com.fiap.mareco.services.mostrarMensagemEmConstrucao
 
 @Composable
 fun HomeScreen(navController: NavController) {
@@ -53,8 +44,8 @@ fun HomeScreen(navController: NavController) {
             }
         }
 
-        val listaReporte = listarReportes(contexto);
-        
+        val listaReporte = listarReportes(contexto)
+
         listaReporte.forEach {
             DestaqueComponent(destaque = it)
         }
